52d2349f3129794df6b765f0f095d588c27e9a3b
GangaMegha <gangamegha29@gmail.com>
2022-11-11 15:33:29 -0500

    correction var schedule and reveese sampling

--- src/diffusion/reverse_data_generate.py
+++ src/diffusion/reverse_data_generate.py
@@ -1,5 +1,6 @@
 '''
 Note : This script is taken from part of https://huggingface.co/blog/annotated-diffusion
+        and modified by Ganga Meghanath
 '''
 
 import torch
@@ -11,12 +12,12 @@ def extract(a, t, x_shape):
     return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
 
 @torch.no_grad()
-def p_sample(model, x, t, t_index, betas, sqrt_recip_alphas, sqrt_one_minus_alphas_cumprod, posterior_variance):
-    betas_t = extract(betas, t, x.shape)
+def p_sample(model, x, t, t_index, variance_dict):
+    betas_t = extract(variance_dict["betas"], t, x.shape)
     sqrt_one_minus_alphas_cumprod_t = extract(
-        sqrt_one_minus_alphas_cumprod, t, x.shape
+        variance_dict["sqrt_one_minus_alphas_cumprod"], t, x.shape
     )
-    sqrt_recip_alphas_t = extract(sqrt_recip_alphas, t, x.shape)
+    sqrt_recip_alphas_t = extract(variance_dict["sqrt_recip_alphas"], t, x.shape)
     
     # Equation 11 in the paper
     # Use our model (noise predictor) to predict the mean
@@ -27,14 +28,14 @@ def p_sample(model, x, t, t_index, betas, sqrt_recip_alphas, sqrt_one_minus_alph
     if t_index == 0:
         return model_mean
     else:
-        posterior_variance_t = extract(posterior_variance, t, x.shape)
+        posterior_variance_t = extract(variance_dict["posterior_variance"], t, x.shape)
         noise = torch.randn_like(x)
         # Algorithm 2 line 4:
         return model_mean + torch.sqrt(posterior_variance_t) * noise 
 
 # Algorithm 2 but save all images:
 @torch.no_grad()
-def p_sample_loop(model, shape, T):
+def p_sample_loop(model, variance_dict, shape, T):
     device = next(model.parameters()).device
 
     b = shape[0]
@@ -49,5 +50,5 @@ def p_sample_loop(model, shape, T):
     return imgs
 
 @torch.no_grad()
-def sample(model, image_size, batch_size=16, channels=3):
-    return p_sample_loop(model, shape=(batch_size, channels, image_size, image_size))
+def sample(model, variance_dict, cfg):
+    return p_sample_loop(model, variance_dict, shape=(cfg.get('batch_size'), cfg.get('channels'), cfg.get('image_size'), cfg.get('image_size')), T=cfg.get('T'))
--- src/diffusion/var_schedule.py
+++ src/diffusion/var_schedule.py
@@ -1,54 +1,72 @@
 '''
-Note : This script is taken from part of https://huggingface.co/blog/annotated-diffusion
-        and modified by Ganga Meghanath
+Note :  This script is initially taken from part of https://huggingface.co/blog/annotated-diffusion
+        and modified by Ganga Meghanath.
 '''
 
 import torch
-from tqdm.auto import tqdm
 
-def extract(a, t, x_shape):
-    batch_size = t.shape[0]
-    out = a.gather(-1, t.cpu())
-    return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
+def cosine_beta_schedule(T, s=0.008):
+    """
+    cosine schedule as proposed in https://arxiv.org/abs/2102.09672
+    """
+    steps = T + 1
+    t = torch.linspace(0, T, steps)
+    ft = torch.cos(((t / T) + s) / (1 + s) * torch.pi * 0.5) ** 2
+    alphas_cumprod = ft / ft[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return torch.clip(betas, 0.0001, 0.9999)
 
-@torch.no_grad()
-def p_sample(model, x, t, t_index, variance_dict):
-    betas_t = extract(variance_dict["betas"], t, x.shape)
-    sqrt_one_minus_alphas_cumprod_t = extract(
-        variance_dict["sqrt_one_minus_alphas_cumprod"], t, x.shape
-    )
-    sqrt_recip_alphas_t = extract(variance_dict["sqrt_recip_alphas"], t, x.shape)
-    
-    # Equation 11 in the paper
-    # Use our model (noise predictor) to predict the mean
-    model_mean = sqrt_recip_alphas_t * (
-        x - betas_t * model(x, t) / sqrt_one_minus_alphas_cumprod_t
-    )
+def linear_beta_schedule(T):
+    beta_start = 0.0001
+    beta_end = 0.02
+    return torch.linspace(beta_start, beta_end, T)
 
-    if t_index == 0:
-        return model_mean
+def quadratic_beta_schedule(T):
+    beta_start = 0.0001
+    beta_end = 0.02
+    return torch.linspace(beta_start**0.5, beta_end**0.5, T) ** 2
+
+def sigmoid_beta_schedule(T):
+    beta_start = 0.0001
+    beta_end = 0.02
+    betas = torch.linspace(-6, 6, T)
+    return torch.sigmoid(betas) * (beta_end - beta_start) + beta_start
+
+
+def alpha_beta(T, schedule="linear"):
+    # define beta schedule
+    if schedule=="cosine":
+        betas = cosine_beta_schedule(T)
+    elif schedule=="linear":
+        betas = linear_beta_schedule(T)
+    elif schedule=="quadratic":
+        betas = quadratic_beta_schedule(T)
+    elif schedule=="sigmoid":
+        betas = sigmoid_beta_schedule(T)
     else:
-        posterior_variance_t = extract(variance_dict["posterior_variance"], t, x.shape)
-        noise = torch.randn_like(x)
-        # Algorithm 2 line 4:
-        return model_mean + torch.sqrt(posterior_variance_t) * noise 
+        print("\n\n\n\t\tVariance Schedule is UNKNOWN!! \n\nPlease choose schedule from one of the following:  \n\tcosine, \n\tlinear, \n\tquadratic, \n\tsigmoid")
+        raise NotImplementedError()
 
-# Algorithm 2 but save all images:
-@torch.no_grad()
-def p_sample_loop(model, variance_dict, shape, T):
-    device = next(model.parameters()).device
+    # define alphas 
+    alphas = 1. - betas
+    alphas_cumprod = torch.cumprod(alphas, axis=0)
+    alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
+    sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
 
-    b = shape[0]
-    # start from pure noise (for each example in the batch)
-    img = torch.randn(shape, device=device)
-    imgs = []
-    
-    for i in tqdm(reversed(range(0, T)), desc='sampling loop time step', total=T):
-        img = p_sample(model, img, torch.full((b,), i, device=device, dtype=torch.long), i)
-        if i%50==0:
-            imgs.append(img.cpu())
-    return imgs
+    # calculations for diffusion q(x_t | x_{t-1}) and others
+    sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+    sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
 
-@torch.no_grad()
-def sample(model, variance_dict, cfg):
-    return p_sample_loop(model, variance_dict, shape=(cfg.get('batch_size'), cfg.get('channels'), cfg.get('image_size'), cfg.get('image_size')), T=cfg.get('T'))
+    # calculations for posterior q(x_{t-1} | x_t, x_0)
+    posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+
+    variance_dict = {
+        "betas" : betas,
+        "alphas_cumprod" : alphas_cumprod,
+        "sqrt_recip_alphas" : sqrt_recip_alphas,
+        "sqrt_alphas_cumprod" : sqrt_alphas_cumprod,
+        "sqrt_one_minus_alphas_cumprod" : sqrt_one_minus_alphas_cumprod,
+        "posterior_variance" : posterior_variance,
+    }
+
+    return variance_dict
