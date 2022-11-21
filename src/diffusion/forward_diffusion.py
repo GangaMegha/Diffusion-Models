e40174f44a3953f1807efe3e1a1821e6e6c25d96
GangaMegha <gangamegha29@gmail.com>
2022-11-21 18:21:24 -0500

    fixed forward diffusion

--- src/diffusion/forward_diffusion.py
+++ src/diffusion/forward_diffusion.py
@@ -13,22 +13,22 @@ def extract(a, t, x_shape):
     return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
 
 # forward diffusion
-def q_sample(x_start, t, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod, noise=None):
+def q_sample(x_start, t, var_dict, noise=None):
     if noise is None:
         noise = torch.randn_like(x_start)
 
-    sqrt_alphas_cumprod_t = extract(sqrt_alphas_cumprod, t, x_start.shape)
+    sqrt_alphas_cumprod_t = extract(var_dict["sqrt_alphas_cumprod"], t, x_start.shape)
     sqrt_one_minus_alphas_cumprod_t = extract(
-        sqrt_one_minus_alphas_cumprod, t, x_start.shape
+        var_dict["sqrt_one_minus_alphas_cumprod"], t, x_start.shape
     )
 
     return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
 
-def get_noisy_image(x_start, t):
+def get_noisy_image(x_start, t, var_dict):
   # add noise
-  x_noisy = q_sample(x_start, t=t)
+  x_noisy = q_sample(x_start, t=t, var_dict=var_dict)
 
   # turn back into [0,255] CHW
-  noisy_image = reverse_transform(x_noisy.squeeze())
+  noisy_image = reverse_transform()(x_noisy.squeeze())
 
   return noisy_image
